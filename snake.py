c64a16ff1f85ba6e2c5c13702c01838b7f00479f
Diego Alejandro Restrepo Sanchez <diegorestrepo68@utp.edu.co>
2021-03-02 23:47:15 -0500

    input function added and refactor de matrix map.

--- snake.py
+++ snake.py
@@ -6,8 +6,11 @@ pygame.init()
 SIZE = 20
 
 #MOVS RIGHT UP LEFT DOWN
-movx = [SIZE, 0, -SIZE, 0]
-movy = [0, -SIZE, 0, SIZE]
+movx = [1, 0, -1, 0]
+movy = [0, -1, 0, 1]
+
+dx = [1, 0, 1, -1, 0, -1, 1, -1]
+dy = [0, 1, 1, 0, -1, -1, -1, 1]
 
 index = {
   pygame.K_RIGHT : 0,
@@ -16,8 +19,13 @@ index = {
   pygame.K_DOWN : 3
 }
 
+INF = 34
+
 class snake():
   def __init__(self, height, width, speed):
+    # self.NN = NN
+    print(width//SIZE, height//SIZE)
+    self.map = [[0]*(width//SIZE) for i in range(height//SIZE)]
     self.height = height
     self.width = width
     self.screen = pygame.display.set_mode((self.width, self.height))
@@ -25,15 +33,17 @@ class snake():
     self.speed = speed
     self.direction = movx[0], movy[0]
     self.score = 0
-    self.head = [(self.height/SIZE//2)*SIZE, (self.width/SIZE//2)*SIZE]
-    self.food = []
+    self.head = [(self.width//SIZE)//2, (self.height//SIZE)//2]
+    self.food = [0, 0]
     self.clock = pygame.time.Clock()
-    self.snake = [self.head,[self.head[0]-SIZE, self.head[1]], [self.head[0]-2*SIZE, self.head[1]]]
+    self.snake = [self.head,[self.head[0]-1, self.head[1]], [self.head[0]-2, self.head[1]]]
+    for pos in self.snake: 
+      self.map[pos[1]][pos[0]] = 1
     #self.screen.fill((200,0,0))
     self.generate_food()
 
   def game(self):
-    directionaux = [0,0]
+    directionaux = self.direction
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         raise SystemExit("Gracias por jugar!")
@@ -43,6 +53,7 @@ class snake():
           raise SystemExit("Gracias por jugar!")
         directionaux = [movx[index[event.key]], movy[index[event.key]]]
 
+    print(self.get_input())
     aux = self.move(directionaux)
     if self.check_move(aux):
       self.direction = directionaux
@@ -51,14 +62,17 @@ class snake():
     self.head = aux
     self.snake.insert(0, self.head)
 
-    if self.end_game():
-      return True, self.score
-
     if self.head == self.food:
       self.score += 1
       self.generate_food()
     else:
+      pos = self.snake[-1]
+      self.map[pos[1]][pos[0]] = 0
       self.snake.pop()
+
+    if self.end_game():
+      return True, self.score
+    self.map[aux[1]][aux[0]] = 1
 
     self.UI()
     self.clock.tick(self.speed)
@@ -68,26 +82,50 @@ class snake():
   def UI(self):
     self.screen.fill((0,0,0))
     for i in self.snake:
-      pygame.draw.rect(self.screen, (90,90,90), pygame.Rect((i[0], i[1]), ([SIZE]*2)));
-    pygame.draw.rect(self.screen, (200,0,0), pygame.Rect((self.food[0], self.food[1]), ([SIZE]*2)));
+      pygame.draw.rect(self.screen, (90,90,90), pygame.Rect((i[0]*SIZE, i[1]*SIZE), ([SIZE]*2)))
+    pygame.draw.rect(self.screen, (200,0,0), pygame.Rect((self.food[0]*SIZE, self.food[1]*SIZE), ([SIZE]*2)))
     pygame.display.flip()
 
   def end_game(self):
     if self.head in self.snake[1:]:
       return True
-    return self.head[0] < 0 or self.head[1] < 0 or self.head[0] >= self.width or self.head[1] >= self.height
+    return self.head[0] < 0 or self.head[1] < 0 or self.head[0] >= self.width//SIZE or self.head[1] >= self.height//SIZE
 
   def check_move(self, position):
-    if position in self.snake:
+    if position == self.snake[1]:
       return False
     return True
 
+  def get_move():
+    pass
+  
+  def get_input(self):
+    ans = [INF]*24
+
+    for i in range(1, max(self.width//SIZE, self.height//SIZE)):
+      for j in range(8):
+        x = self.head[0]+dx[j]*i
+        y = self.head[1]+dy[j]*i
+
+        if(self.check_range(x, y) and self.map[y][x]):
+          ans[self.map[y][x]*8 + j] = min(ans[self.map[y][x]*8 + j], i)
+
+        if(not self.check_range(x, y)):
+          ans[j] = min(ans[j], i)
+    
+    return ans
+
+  def check_range(self, x, y):
+    return 0 <= x < self.width//SIZE and 0 <= y < self.height//SIZE
+
   def generate_food(self):
-    x = random.randint(0, (self.width/SIZE - 1))
-    y = random.randint(0, (self.height/SIZE - 1))
-    self.food = [x*SIZE, y*SIZE]
-    if self.food in self.snake:
-      self.generate_food()
+    while True:
+      x = random.randint(0, (self.width//SIZE - 1))
+      y = random.randint(0, (self.height//SIZE - 1))
+      self.food = [x, y]
+      if not self.food in self.snake:
+        self.map[y][x] = 2
+        break
 
   def move(self, direction):
     x = self.head[0] + direction[0]
@@ -102,4 +140,4 @@ if __name__ == "__main__":
     final, score = game.game()
     if final == True:
       break
-  print("Score: ", score);
+  print("Score: ", score)
